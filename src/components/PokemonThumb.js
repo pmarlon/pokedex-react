502d0a5ec0eb6257015d34a3003f0551dbff76d7
Patric Marlon <pmarlon@pop-os.localdomain>
2022-04-04 22:48:07 -0300

    initial commit

---
+++ src/components/PokemonThumb.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const PokemonThumb = ({id, name, image, type, _callback}) => {
+  const style = type + " thumb-container";
+  return (
+    <div className={style}>
+        <div className='number'>
+            <small>#0{id}</small>
+        </div>
+        <img src={image} alt={name} />
+        <div className='detail-wrapper'>
+            <h3>{name}</h3>
+            <small>Type: {type}</small>
+        </div>
+
+    </div>
+  )
+}
+
+export default PokemonThumb
